0c059d20669e5f0e92d68d8012104e2d97b87876
Joshua E Bearden <josh.e.bearden@gmail.com>
2017-12-05 16:11:43 -0600

    Wrote basic structure for runsim and created testing data file

---
+++ runsim.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[])
+{
+   pid_t pid = 0;
+   pid_t wait = 0;
+   int i, pr_limit, pr_count, status;
+   pr_count = 0;
+   char * str;
+
+   if(argc != 2)
+   {
+      printf("Wrong number of command line arguments!\n");
+      return 1;
+   }
+
+   pr_limit = atoi(argv[1]);
+
+   fgets(str, MAX_BUF, stdin);
+
+   while(str != NULL)
+   {
+      if(pr_count == pr_limit)
+      {
+         wait = waitpid(-1, &status, WNOHANG);
+         while(wait == 0)
+         {
+            wait = waitpid(-1, &status, WNOHANG);
+         }
+         printf("Process %d exited with status %d\n", wait, status);
+         pr_count--;
+      }
+      
+      pid = fork();
+      
+      if(pid == 0)
+      {
+         char * args[3];
+         args[0] = strtok(str, " ");
+         for(i = 1; i < 3; i++)
+         {
+            args[i] = strtok(NULL, " ");
+         }
+
+         execv(args[0], args);
+      } 
+      else
+      {
+         pr_count++;
+         wait = waitpid(-1, &status, WNOHANG);
+         if (wait != 0)
+         {
+            pr_count--;
+            printf("Process %d exited with status %d\n", wait, status);
+         }
+      }
+   }
+   while (pr_count > 0)
+   {
+      wait = waitpid(-1, &status, WNOHANG);
+      if(wait != 0)
+      {
+         pr_count--;
+         printf("Process %d exited with status %d\n", wait, status);
+      }
+   }
+}
