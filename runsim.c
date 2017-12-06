5feee9525ce3618ae42b57a347231b67e6238119
Joshua E Bearden <josh.e.bearden@gmail.com>
2017-12-06 17:45:22 -0600

    added comments

--- runsim.c
+++ runsim.c
@@ -1,3 +1,8 @@
+/*
+ * Joshua Bearden
+ * CS2750 PA6
+ * 05/12/17
+ */
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -6,30 +11,40 @@
 
 #define MAX_BUF 200
 
+//A program to create child processes running concurrently
+//that run a given command (with arguments) from stdin
+
 int main(int argc, char *argv[])
 {
-   pid_t pid = 0;
-   pid_t wait = 0;
+   pid_t pid = 0; //process ID so we know whether parent or child
+   pid_t wait = 0; //holds the process ID for the last terminated process
    int i, pr_limit, pr_count, status;
    pr_count = 0;
-   char str[MAX_BUF];
+   char str[MAX_BUF]; //Holds the command to be executed and its args
    char * done;
 
+   //Ensure the program was executed correctly
    if(argc != 2)
    {
       printf("Wrong number of command line arguments!\n");
       return 1;
    }
 
+   //Set the max number of concurrent processes
    pr_limit = atoi(argv[1]);
 
+   //read the first line of input from stdin
    done = fgets(str, MAX_BUF, stdin);
 
+   //main loop
    while(done != NULL)
    {
       printf("Command is: %s\n", str);
+
+      //check if we are running the max number of processes
       if(pr_count == pr_limit)
       {
+         //wait for one to terminate
          wait = waitpid(-1, &status, WNOHANG);
          while(wait == 0)
          {
@@ -39,8 +54,10 @@ int main(int argc, char *argv[])
          pr_count--;
       }
       
+      //make a new child process
       pid = fork();
-      
+
+      //if this is the child, get the command and args to run and exec      
       if(pid == 0)
       {
          char * args[3];
@@ -52,7 +69,7 @@ int main(int argc, char *argv[])
 
          execv(args[0], args);
       } 
-      else
+      else //if this is the parent, increment num of processes & check if a process terminated
       {
          pr_count++;
          wait = waitpid(-1, &status, WNOHANG);
@@ -64,6 +81,8 @@ int main(int argc, char *argv[])
       }
       done = fgets(str, MAX_BUF, stdin);
    }
+
+   //Once the input has been exhausted, wait for each process to finish
    while (pr_count > 0)
    {
       wait = waitpid(-1, &status, WNOHANG);
